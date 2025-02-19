483d85695c2bac40ff2479e9e20a029bd97f6e03
chaan3 <chandaji628@gmail.com>
2025-02-19 13:30:40 +0530

    PAYPAL updated

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -1,9 +1,15 @@
 const paypal = require("paypal-rest-sdk");
 
+// paypal.configure({
+//   mode: "",
+//   client_id: "",
+//   client_secret: "",
+// });
 paypal.configure({
-  mode: "",
-  client_id: "",
-  client_secret: "",
+  mode: "sandbox", // or "live"
+  client_id: "dummy_client_id",
+  client_secret: "dummy_client_secret"
 });
+
 
 module.exports = paypal;

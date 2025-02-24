9485b1dedd8f47bb7713e647828d3b2e17a820b1
chaan3 <chandaji628@gmail.com>
2025-02-24 15:09:48 +0530

    update API endpoints to include '/api' prefix for authentication requests

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -12,7 +12,7 @@ export const registerUser = createAsyncThunk(
 
   async (formData) => {
     const response = await axios.post(
-      "https://trendz-eshopping-backend.onrender.com/auth/register",
+      "https://trendz-eshopping-backend.onrender.com/api/auth/register",
       formData,
       {
         withCredentials: true,
@@ -28,7 +28,7 @@ export const loginUser = createAsyncThunk(
 
   async (formData) => {
     const response = await axios.post(
-      "https://trendz-eshopping-backend.onrender.com/auth/login",
+      "https://trendz-eshopping-backend.onrender.com/api/auth/login",
       formData,
       {
         withCredentials: true,
@@ -44,7 +44,7 @@ export const logoutUser = createAsyncThunk(
 
   async () => {
     const response = await axios.post(
-      "https://trendz-eshopping-backend.onrender.com/auth/logout",
+      "https://trendz-eshopping-backend.onrender.com/api/auth/logout",
       {},
       {
         withCredentials: true,
@@ -60,7 +60,7 @@ export const checkAuth = createAsyncThunk(
 
   async () => {
     const response = await axios.get(
-      "https://trendz-eshopping-backend.onrender.com/auth/check-auth",
+      "https://trendz-eshopping-backend.onrender.com/api/auth/check-auth",
       {
         withCredentials: true,
         headers: {

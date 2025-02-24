401d2aaff4db25ed791bc47afe6550ce1a2ab8f2
chaan3 <chandaji628@gmail.com>
2025-02-24 15:40:39 +0530

    fix: update API endpoints to use localhost for development

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -12,7 +12,7 @@ export const registerUser = createAsyncThunk(
 
   async (formData) => {
     const response = await axios.post(
-      "https://trendz-eshopping-backend.onrender.com/api/auth/register",
+      "https://localhost:5000/api/auth/register",
       formData,
       {
         withCredentials: true,
@@ -28,7 +28,7 @@ export const loginUser = createAsyncThunk(
 
   async (formData) => {
     const response = await axios.post(
-      "https://trendz-eshopping-backend.onrender.com/api/auth/login",
+      "https://localhost:5000/api/auth/login",
       formData,
       {
         withCredentials: true,
@@ -44,7 +44,7 @@ export const logoutUser = createAsyncThunk(
 
   async () => {
     const response = await axios.post(
-      "https://trendz-eshopping-backend.onrender.com/api/auth/logout",
+      "https://localhost:5000/api/auth/logout",
       {},
       {
         withCredentials: true,
@@ -60,7 +60,7 @@ export const checkAuth = createAsyncThunk(
 
   async () => {
     const response = await axios.get(
-      "https://trendz-eshopping-backend.onrender.com/api/auth/check-auth",
+      "https://localhost:5000/api/auth/check-auth",
       {
         withCredentials: true,
         headers: {
